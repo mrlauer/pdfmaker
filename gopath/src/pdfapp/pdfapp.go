ab0b19cafeb37419f302e89a574707afac1a6f24
Michael Lauer <michael@mrlauer.com>
2012-02-27 07:35:40 -0500

    change style

--- gopath/src/pdfapp/pdfapp.go
+++ gopath/src/pdfapp/pdfapp.go
@@ -173,7 +173,6 @@ func DefaultDocument() *Document {
 }
 
 func writeDoc(w http.ResponseWriter, doc *Document) {
-	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(doc)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
